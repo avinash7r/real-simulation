46278c746108f58bf4fa4c1fd58fbd7f1956f40b
Avinash Rajure <rajureavinash7@gmail.com>
2026-06-17 08:32:53 +0530

    Add SSL configuration to PostgreSQL connection pool

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const { Pool } = require("pg");
 const app = express();
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 });
 
 pool.on("connect", () => console.log("[db] new connection opened"));
